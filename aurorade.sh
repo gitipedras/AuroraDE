e62977dea01ff7bc7b69f4f9d4d41dc2a21a4b1a
Ian Pedras <ian@pedras.org>
2024-05-07 15:44:24 +0100

    v1.0.0

---
+++ aurorade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme Yaru
+
+kwin &
+latte-dock &
+plank &
+
---
+++ install-aurorade.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
+
+sudo apt-get install -y latte-dock kwin plank li sddm gnome-terminal gnome-themes-extra gnome-themes-extra-data gnome-tweaks gnome-todo gnome-sound-recorder gnome-software-plugin-flatpak gnome-software gnome-software-common gnome-system-monitor  git
+
+git clone https://github.com/ubuntu/yaru.git
+
+mv yaru /usr/share/themes
+
+mkdir Desktop Documents Downloads Favorites
+
+sudo mv aurorade.desktop /usr/share/xsessions
